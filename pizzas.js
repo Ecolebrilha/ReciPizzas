cd52264daf8330180fd4aa2eb10704a039b40ea3
Ecolebrilha <144856681+Ecolebrilha@users.noreply.github.com>
2024-03-21 21:32:04 -0300

    Add files via upload

---
+++ pizzas.js
@@ -0,0 +1,158 @@
+let pizzaJson = [
+    {
+        id: 1,
+        name: 'Mussarela',
+        img: 'images/pizza.png',
+        alt: 'pizza-1',
+        price: [20.00, 23.00, 25.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, camada dupla de mussarela e orégano'
+      },
+      {
+        id: 2,
+        name: 'Calabresa',
+        img: 'images/pizza2.png',
+        alt: 'pizza-2',
+        price: [21.00, 24.00, 26.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, mussarela, calabresa fatiada, cebola e orégano'
+      },
+      {
+        id: 3,
+        name: 'Quatro Queijos',
+        img: 'images/pizza3.png',
+        alt: 'pizza-3',
+        price: [23.00, 26.00, 28.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, camadas de mussarela, provolone, parmessão, gorgonzola e orégano'
+      },
+      {
+        id: 4,
+        name: 'Brasileira',
+        img: 'images/pizza4.png',
+        alt: 'pizza-4',
+        price: [25.00, 28.00, 30.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, mussarela, calabresa picada, palmito, champignon, azeitonas e orégano'
+      },
+      {
+        id: 5,
+        name: 'Portuguesa',
+        img: 'images/pizza5.png',
+        alt: 'pizza-5',
+        price: [24.00, 27.00, 29.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, mussarela, presunto, ovos, cebolas, pimentão, azeitona e orégano'
+      },
+      {
+        id: 6,
+        name: 'Moda da Casa',
+        img: 'images/pizza6.png',
+        alt: 'pizza-6',
+        price: [30.00, 33.00, 35.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, mussarela, carne de sol, tomates em cubos, coentro, cebola, azeitona, catupiry e orégano'
+      },
+      {
+        id: 7,
+        name: 'Banana com canela',
+        img: 'images/pizza7.png',
+        alt: 'pizza-7',
+        price: [27.00, 30.00, 32.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Mussarela, banana, canela e açúcar'
+      },
+      {
+        id: 8,
+        name: 'Chocolate com morango',
+        img: 'images/pizza8.png',
+        alt: 'pizza-8',
+        price: [30.00, 32.00, 35.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Creme de leite, lascas de chocolate e morangos'
+      },
+      {
+        id: 9,
+        name: 'Marguerita',
+        img: 'images/pizza9.png',
+        alt: 'pizza-9',
+        price: [23.00, 26.00, 28.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, mussarela, alho em lascas, tomate cereja, manjericão e orégano'
+      },
+      {
+        id: 10,
+        name: 'Bacon',
+        img: 'images/pizza10.png',
+        alt: 'pizza-10',
+        price: [25.00, 28.00, 30.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Mussarela, bacon, orégano e azeitona'
+      },
+      {
+        id: 11,
+        name: 'Frango com Catupiry',
+        img: 'images/pizza11.png',
+        alt: 'pizza-11',
+        price: [21.00, 24.00, 26.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, mussarela, frango, catupiry e orégano'
+      },
+      {
+        id: 12,
+        name: 'Lombiry',
+        img: 'images/pizza12.png',
+        alt: 'pizza-12',
+        price: [28.00, 31.00, 33.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, mussarela, lombo canadense, catupiry e orégano.'
+      },
+];
